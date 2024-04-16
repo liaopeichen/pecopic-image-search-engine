efd8baa41694cf5857cce15ba6389b36ceed7e60
廖珮辰 <152478331+liaopeichen@users.noreply.github.com>
2024-04-16 17:37:20 +0800

    feat: implement additional image functionality

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,9 +4,11 @@ import SearchInput from "../components/SearchInput";
 import Image from "../components/Image";
 
 export default function Index() {
-  const [searchInput, setSearchInput] = useState("");
+  const [page, setPage] = useState(1);
   const [photos, setPhotos] = useState(null);
-  const initialURL = "https://api.pexels.com/v1/curated?per_page=1&per_page=15";
+  const [searchInput, setSearchInput] = useState("");
+  const [currentSearch, setCurrentSearch] = useState("");
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   let searchURL = `https://api.pexels.com/v1/search?query=${searchInput}&locale=zh-TW&locale=en-US&page=1&per_page=15`;
 
   const search = async function (url) {
@@ -15,6 +17,24 @@ export default function Index() {
     });
 
     setPhotos(result.data.photos);
+    setCurrentSearch(searchInput);
+  };
+
+  const moreImagesHandler = async function () {
+    let newURL;
+    setPage((currentPage) => currentPage + 1);
+
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&locale=zh-TW&locale=en-US&page=${page}&per_page=15`;
+    }
+
+    let result = await axios.get(newURL, {
+      headers: { Authorization: import.meta.env.VITE_PEXELS_API_KEY },
+    });
+
+    setPhotos(photos.concat(result.data.photos));
   };
 
   useEffect(() => {
@@ -32,6 +52,9 @@ export default function Index() {
           photos.map((photo) => {
             return <Image key={photo.id} photo={photo} />;
           })}
+      </div>
+      <div className="moreImages">
+        <button onClick={moreImagesHandler}>更多圖片</button>
       </div>
     </div>
   );
