25e64734c7c3e3795591e9e3d1ab4c9d3f0762dd
廖珮辰 <152478331+liaopeichen@users.noreply.github.com>
2024-04-16 10:39:22 +0800

    feat: fetch photos data using axios package

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function SearchInput() {
+  const [data, setData] = useState(null);
+  const initialURL =
+    "https://api.pexels.com/v1/search?query=nature&page=1&per_page=15";
+
+  const searchButtonHandler = async function () {
+    let result = await axios.get(initialURL, {
+      headers: { Authorization: import.meta.env.VITE_PEXELS_API_KEY },
+    });
+    setData(result.data.photos);
+  };
+
   return (
     <div className="search">
       <input className="input" type="text" />
-      <button>Search</button>
+      <button onClick={searchButtonHandler}>Search</button>
     </div>
   );
 }
